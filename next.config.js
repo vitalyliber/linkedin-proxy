748575c76ac2f884fe168c64675b53a18915e209
vitaly.liber <zenamax@gmail.com>
2021-07-19 12:38:20 +0300

    add config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/proxy/:path*',
+        destination: 'https://www.linkedin.com/:path*',
+      },
+    ]
+  },
 }
